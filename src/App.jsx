2e3931f894949927798bc8ea8f04f257b6be7431
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-08-11 13:29:05 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'modern-normalize';
+import './App.css';
+import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
+import ContactForm from './components/ContactForm/ContactForm';
+
+const App = () => {
+
+  return (
+    <div className='box'>
+      <h1>Phone book</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+};
+
+export default App;
