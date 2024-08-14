f33a91a1b6a0eeff8caf8564963061dce9458514
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-08-14 21:45:52 +0300

    img

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,10 @@
 import s from './HomePage.module.css'
-
+import img from '../../images/phoneBookImg.jpg';
 const HomePage = () => {
     return (
         <div className={s.wrapper}>
             <h1>Welcome to the <span>phone book</span> app!</h1>
-            <img src="../../../images/phoneBookImg.jpeg" alt="" />
+            <img src={img} alt="Phone Book" />
         </div>
     )
 };
