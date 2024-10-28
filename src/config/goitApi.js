c00548721b832c7279e00a97e1e1aeb20abc4f0e
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-10-28 16:54:38 +0200

    up

--- src/config/goitApi.js
+++ src/config/goitApi.js
@@ -3,11 +3,13 @@ import axios from "axios";
 // export const goitApi = axios.create({
 //     baseURL: 'https://connections-api.goit.global/',
 // })
+// export const goitApi = axios.create({
+//     baseURL: 'https://nodejs-hw-mongodb-ztyo.onrender.com',
+// })
+
 export const goitApi = axios.create({
-    baseURL: 'https://nodejs-hw-mongodb-ztyo.onrender.com',
+    baseURL: 'http://localhost:3000',
 })
-
-
 export const setAuthHeader = (token) => {
     goitApi.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
